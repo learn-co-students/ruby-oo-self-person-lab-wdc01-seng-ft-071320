24b93216c0482f4cc80193b2f20e602f5974fa02
JinaStrickland <Jina30@gmail.com>
2020-07-15 19:43:05 -0400

    completed lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,90 @@
 # your code goes here
+require 'pry'
+class Person
+
+attr_reader :name,  :happiness, :hygiene
+attr_accessor :bank_account
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+  
+  def happiness=(new_happiness)
+    if new_happiness > 10 
+        @happiness = 10
+    elsif new_happiness < 0
+        @happiness = 0
+    else
+        @happiness = new_happiness
+    end
+  end
+
+  def hygiene=(new_hygiene)
+    if new_hygiene > 10
+        @hygiene = 10
+    elsif new_hygiene < 0
+        @hygiene = 0
+    else
+        @hygiene = new_hygiene
+    end
+  end
+
+  def clean?
+     @hygiene > 7
+  end
+
+  def happy?
+    @happiness > 7 
+  end
+  
+  def get_paid(salary_amount)
+    @bank_account += salary_amount 
+    "all about the benjamins"
+  end 
+  
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+   
+  end
+
+  def work_out
+    self.happiness += 2
+    self.hygiene -= 3
+    # self.happiness=(@happiness)
+    # self.hygiene=(@hygiene)
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    # self.happiness=(@happiness)
+    # friend.happiness=(friend.happiness)
+     "Hi #{friend.name}! It's #{self.name}. How are you?"
+     
+  end
+
+  def start_conversation(person,topic)
+    if topic == "politics"
+      self.happiness -= 2
+      person.happiness -= 2
+      "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      self.happiness += 1
+      person.happiness += 1
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
+  end
+  
+end
+
+# john = Person.new("John")
+# doug = Person.new("Doug")
+# john.take_bath
+# puts john.clean?
