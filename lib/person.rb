b24c66a8c40719a66d1ec23084e446655df518da
chsohn15 <chsohn15@gmail.com>
2020-07-15 14:02:49 -0400

    created person and initialized

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,30 @@
 # your code goes here
+require 'pry'
+
+class Person
+  
+attr_reader :name, :happiness, :hygiene
+attr_accessor :bank_account
+
+    def initialize(name)
+        
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+        
+    end
+
+    def happiness=(happiness)
+        #Check to see if happiness is greater than ten or less that zero
+        #If so, do not change happiness
+    end
+
+    def hygiene= (hygiene)
+
+    end
+
+
+end
+
+
