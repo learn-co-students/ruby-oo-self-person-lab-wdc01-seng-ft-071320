4fd5daeaa227797da340429326f5e76b193246ae
Rbascoe <roanbascoe5@gmail.com>
2020-07-15 18:57:29 -0400

    Almost done

--- lib/person.rb
+++ lib/person.rb
@@ -1,24 +1,88 @@
+require 'pry'
 # your code goes here
 class Person
 
-    attr_reader :name, :money, :happ_points, :hyg_points
+    attr_accessor :bank_account, :happiness, :hygiene
     # def name=(name)
     #     @name = name
     # end
-    
+    attr_reader :name
 
 
-    def initialize(name, money, happ_points=8, hyg_points=8)
+
+    def initialize(name)
         @name = name
-        @money = 25
-        @happ_points = happ_points
-        @hyg_points = hyg_points
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end 
+
+    def happiness=(num)
+        @happiness = num
+        if @happiness >= 10
+            @happiness = 10
+        elsif @happiness <= 0
+            @happiness = 0
+        end
     end
 
-    def 
+    def hygiene=(num)
+        @hygiene = num
+        if @hygiene >= 10
+            @hygiene = 10
+        elsif @hygiene <= 0
+            @hygiene = 0
+        end
+    end
+
+    def bank_account=(num)
+        @bank_account = num
+    end
+
+    def happy?
+         @happiness > 7 
+    end
+
+    def clean?
+        @hygiene > 7 
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        bath = @hygiene += 4
+        self.hygiene=(bath)
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        work = @happiness += 2
+        dirty = @hygiene -= 3
+        self.happiness=(work)
+        self.hygiene=(dirty)
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        call = @happiness += 3
+        friend = friend.happiness += 3 
+        return self.happiness=(call)
+        return self.happiness=(friend)
+        if friend 
+            "Hi Felix! It's Stella. How are you?"
+        else
+            "Hi Penelope! It's Felix. How are you?"
+        end
+    end
+    
+
+ end 
 
 
-end 
 
-roan = Person.new("Roan")
-puts roan.money 
+
+
+
