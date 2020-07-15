d41b2babaf801ff6663a26a958e76f7705780e4e
slee713 <steven.lee1393@gmail.com>
2020-07-15 14:08:15 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -51,10 +51,29 @@ class Person
     end
 
     def work_out
-        
+        self.hygiene= (self.hygiene - 3)
+        self.happiness= (self.happiness + 2)
+        return "♪ another one bites the dust ♫"
     end
     
-    def call_friend
+    def call_friend(friend)
+        friend.happiness= (friend.happiness + 3)
+        self.happiness= (self.happiness + 3)
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        if topic == "politics"
+            person.happiness= (person.happiness - 2)
+            self.happiness= (self.happiness - 2)
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            person.happiness= (person.happiness + 1)
+            self.happiness= (self.happiness + 1)
+            return "blah blah sun blah rain"
+        else
+            return "blah blah blah blah blah"
+        end
     end
 
 end
