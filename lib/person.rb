2d9b1e0c67fe99d026ffb1d002c18b2107c71533
Rbascoe <roanbascoe5@gmail.com>
2020-07-15 14:19:40 -0400

    initial commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
 # your code goes here
+class Person
+
+    attr_reader :name, :money, :happ_points, :hyg_points
+    # def name=(name)
+    #     @name = name
+    # end
+    
+
+
+    def initialize(name, money, happ_points=8, hyg_points=8)
+        @name = name
+        @money = 25
+        @happ_points = happ_points
+        @hyg_points = hyg_points
+    end
+
+    def 
+
+
+end 
+
+roan = Person.new("Roan")
+puts roan.money 
