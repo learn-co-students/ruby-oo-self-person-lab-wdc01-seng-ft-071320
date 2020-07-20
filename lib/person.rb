ce0ad0f08d115c55389ac6362bd6a74c2feb29f1
Abdullah Albasha <Abdullah.a.albasha@gmail.com>
2020-07-19 23:10:51 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,3 @@
-# your code goes here
-
-<<<<<<< HEAD
-require 'pry'
-=======
-class Person 
->>>>>>> 9ad2af9f6f22b794a39b3f71aea9a018f251093f
-
 class Person 
 
     attr_reader :name , :happiness, :hygiene
